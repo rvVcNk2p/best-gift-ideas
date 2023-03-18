47618224af614589fe8a534970bc64a2ac3f8043
Kussy Leo <kussynorbertleo@gmail.com>
2023-03-18 10:50:12 +0100

    feat(app): .ico and Logo added

--- components/molecules/nav/Header.vue
+++ components/molecules/nav/Header.vue
@@ -33,7 +33,7 @@
 					<span class="sr-only">Gifty.land</span>
 					<nuxt-img
 						class="h-8 w-8"
-						src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+						src="/gifty-land-logo.svg"
 						alt=""
 						loading="lazy"
 					/>
--- plugins/ga.client.ts
+++ plugins/ga.client.ts
@@ -4,8 +4,8 @@ import { transformBlogPath } from '@libs/string'
 // TODO: Page rename need to be more sophisticated
 const pageName = (path: string) => {
 	if (path === '/') return 'Gifty.land | Home'
-	else if (path === '/blog/') return 'Gifty.land | Blog'
 	else if (path.includes('/blog/en')) return transformBlogPath(path)
+	else if (path.includes('/blog/')) return 'Gifty.land | Blog'
 	else return `${path}`
 }
 
