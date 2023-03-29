649a5dac30d7809102ec34cbb73405fffc798e6e
Kussy Leo <kussynorbertleo@gmail.com>
2023-03-29 02:31:56 +0200

    feat(app): Content fetch structure changed + Published to Vercel

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -8,65 +8,59 @@
 				thoughtful gift ideas that are sure to impress.
 			</p>
 		</div>
-		<ContentList :query="query">
-			<template #not-found>
-				<p>No article yet.</p>
-			</template>
-			<template v-slot="{ list }">
-				<div class="mb-10 flex gap-4">
-					<p
-						v-for="tag in tags(list)"
-						class="tag"
-						:class="{ 'is-selected': tag === selectedTags }"
-						@click="setSelectedTag(tag)"
-					>
-						{{ tag }}
-					</p>
-				</div>
-				<div class="grid grid-cols-1 gap-8 lg:grid-cols-2 2xl:grid-cols-3">
+
+		<div class="mb-10 flex gap-4">
+			<p
+				v-for="tag in tags(data)"
+				class="tag"
+				:class="{ 'is-selected': tag === selectedTags }"
+				@click="setSelectedTag(tag)"
+			>
+				{{ tag }}
+			</p>
+		</div>
+		<div class="grid grid-cols-1 gap-8 lg:grid-cols-2 2xl:grid-cols-3">
+			<div
+				v-for="article in data"
+				:key="article._path"
+				class="flex max-w-sm transform flex-col overflow-hidden rounded-md shadow-xl transition duration-300 hover:scale-105 hover:shadow-2xl"
+			>
+				<nuxt-link :to="`${article._path}`">
 					<div
-						v-for="article in list"
-						:key="article._path"
-						class="flex max-w-sm transform flex-col overflow-hidden rounded-md shadow-xl transition duration-300 hover:scale-105 hover:shadow-2xl"
+						class="width-full flex aspect-video items-center justify-center overflow-hidden"
 					>
-						<nuxt-link :to="`${article._path}`">
-							<div
-								class="width-full flex aspect-video items-center justify-center overflow-hidden"
-							>
-								<nuxt-img
-									class="h-auto w-auto"
-									v-if="article.image"
-									:src="'/img/' + article.image"
-									:alt="article.image"
-									loading="lazy"
-								/>
-							</div>
-							<div class="width-full relative aspect-video overflow-hidden p-6">
-								<h2 class="mb-4 text-xl font-medium">{{ article.title }}</h2>
-								<!-- <p class="line-clamp-3 mb-4 px-1 text-justify italic">
+						<nuxt-img
+							class="h-auto w-auto"
+							v-if="article.image"
+							:src="'/img/' + article.image"
+							:alt="article.image"
+							loading="lazy"
+						/>
+					</div>
+					<div class="width-full relative aspect-video overflow-hidden p-6">
+						<h2 class="mb-4 text-xl font-medium">{{ article.title }}</h2>
+						<!-- <p class="line-clamp-3 mb-4 px-1 text-justify italic">
 									{{ article.description }}
 								</p> -->
-								<div
-									class="absolute bottom-6 right-6 text-sm text-gray-600"
-									v-if="isValid(article.date)"
-								>
-									<ClientOnly>
-										<div class="first-letter:uppercase">
-											{{ formatDateFromNow(article.date) }} ({{
-												formatDateAbsolute(article.date)
-											}})
-										</div>
-										<template #fallback>
-											({{ formatDateAbsolute(article.date) }})
-										</template>
-									</ClientOnly>
+						<div
+							class="absolute bottom-6 right-6 text-sm text-gray-600"
+							v-if="isValid(article.date)"
+						>
+							<ClientOnly>
+								<div class="first-letter:uppercase">
+									{{ formatDateFromNow(article.date) }} ({{
+										formatDateAbsolute(article.date)
+									}})
 								</div>
-							</div>
-						</nuxt-link>
+								<template #fallback>
+									({{ formatDateAbsolute(article.date) }})
+								</template>
+							</ClientOnly>
+						</div>
 					</div>
-				</div>
-			</template>
-		</ContentList>
+				</nuxt-link>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -81,15 +75,6 @@ definePageMeta({
 	layout: 'blog',
 })
 
-const tags = (list: any) => {
-	return list
-		.reduce((acc: string[], nextVal: any) => {
-			acc = [...acc, ...nextVal.tags]
-			return [...new Set(acc)]
-		}, [])
-		.sort()
-}
-
 const selectedTags = ref('')
 
 const setSelectedTag = (tag: string) => {
@@ -98,15 +83,31 @@ const setSelectedTag = (tag: string) => {
 		: (selectedTags.value = tag)
 }
 
-const query = computed(() => ({
-	path: '/',
-	limit: 10,
-	sort: { date: -1 },
-	where:
-		process.env.NODE_ENV === 'development'
-			? { tags: { $contains: selectedTags.value } }
-			: { tags: { $contains: selectedTags.value }, draft: { $ne: true } },
-}))
+const { data } = await useAsyncData(
+	'/',
+	() =>
+		queryContent('/')
+			.limit(10)
+			.sort({ date: -1 })
+			.where(
+				process.env.NODE_ENV === 'development'
+					? { tags: { $contains: selectedTags.value } }
+					: { tags: { $contains: selectedTags.value }, draft: { $ne: true } },
+			)
+			.find(),
+	{
+		watch: [selectedTags],
+	},
+)
+
+const tags = (list: any) => {
+	return list
+		?.reduce((acc: string[], nextVal: any) => {
+			acc = [...acc, ...nextVal.tags]
+			return [...new Set(acc)]
+		}, [])
+		.sort()
+}
 
 useHead({
 	title: 'Gifty.land | Blog',
